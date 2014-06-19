38e6b26e8d9ae0ccb60e61690ca3e640c79bc30b
Daniel Blaszczyk <dblaszc@searshc.com>
2014-06-19 13:26:02 -0500

    Added D3Account Class

---
+++ D3 Info/src/main/java/com/dblas/d3info/app/D3Account.java
@@ -0,0 +1,16 @@
+package com.dblas.d3info.app;
+
+/**
+ * Created by dblaszc on 6/19/2014.
+ */
+public class D3Account {
+    private String mAccountName;
+
+    public String getmAccountName() {
+        return mAccountName;
+    }
+
+    public void setmAccountName(String mAccountName) {
+        this.mAccountName = mAccountName;
+    }
+}
